f7724e87cd7b8c15b43888da924cbe44b6064f1c
Kendal Harland <kharland2011@gmail.com>
2015-02-16 13:46:04 -0600

    added commands in setup.sh to initialize the repository after cloning

--- setup.sh
+++ setup.sh
@@ -1,2 +1,6 @@
 # Add rentalcar source libraries and extra libraries to classpath
 export CLASSPATH=$(pwd)/src:$(pwd)/lib:$CLASSPATH
+
+# Initialize and update any submodules used for this project
+git submodule init
+git submodule update
