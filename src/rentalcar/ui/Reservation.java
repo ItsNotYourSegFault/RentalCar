515b7c150e0cb2fa01d9b5c65a62f6a203b916df
ssammons <s669s142@ku.edu>
2015-02-16 11:29:22 -0600

    added subfolder rentalcar/ui and add file Reservation.java

---
+++ src/rentalcar/ui/Reservation.java
@@ -0,0 +1,31 @@
+
+public class Reservation 
+{
+
+	public Reservation(int _id, String _sDat, ...) {
+		this.id = _id;
+		this.
+	}
+	
+	public Reservation() {}
+	
+	//public String Serialize() {}
+	
+
+	
+	public int id;
+	public String sDat;  
+	public String eDate;
+	public int vin;
+	public int custID;
+	public double price;
+	public int salesID;
+	public int sMiles;
+	public int eMiles;
+	
+}
+
+//Reservation myRes = new Reservation();
+//Reservation myRes = new Reservation(id, sDat, eDate, vin, custID, price, salesID, sMiles, eMiles);
+
+ 
