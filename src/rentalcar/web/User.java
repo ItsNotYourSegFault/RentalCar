90c20b4a3587b140311ad0a21712fa40308cc183
Kendal Harland <kharland2011@gmail.com>
2015-02-25 13:10:18 -0600

    implemented user login

--- src/rentalcar/web/User.java
+++ src/rentalcar/web/User.java
@@ -8,30 +8,59 @@ package rentalcar.web;
   User API Client library for the rental car application.
 */
 
-import org.json.JSONObject;
-import org.json.JSONArray;
 import java.util.List;
 import java.util.ArrayList;
 import rentalcar.web.Request;
 import rentalcar.web.JSONRequest;
 import rentalcar.data.FormObject;
+import org.json.JSONObject;
+import org.json.JSONArray;
 
 public class User {
 
-  private JSONObject user = null;
+  private JSONObject _user = null;
 
   /*
-    bool LogIn
+    boolean LogIn
     @desc  Find the user whose credentials match username and password
     @param user  Form with the user's login credentials
     @return upon success, a JSONObject representing a user is returned. else
             an empty JSON object is returned.
   */
-  public static bool LogIn(FormObject user) {
+  public boolean LogIn(FormObject user) {
     JSONRequest request = new JSONRequest();
-    JSONObject info = request.POST("user/login/", user.ToQueryString());
-    userInfo = info;
+    System.out.println(user.ToQueryString());
+    JSONObject response = request.POST("user/login/", user.ToQueryString());
+    if (response.length() == 0)
+      return false;
+    _user = response;
     return true;
+  }
+
+  /*
+    boolean IsLoggedIn
+    @desc  Return true if the user is logged in, else false.
+  */
+  public boolean IsLoggedIn() { return _user != null; }
+
+  /*
+    String FirstName
+    @desc Get the User's first name
+    @param key  The name of the property
+    @return upon success, the value associated with key. else the empty string
+  */
+  public String FirstName() { 
+    return (String)_user.get("firstname"); 
+  }
+
+  /*
+    String GetLastName
+    @desc Get the User's first name
+    @param key  The name of the property
+    @return upon success, the value associated with key. else the empty string
+  */
+  public String LastName() { 
+    return (String)_user.get("lastname"); 
   }
 
   /*
