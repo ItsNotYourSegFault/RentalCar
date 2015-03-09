b2f2a878339f2ca204aab0519966e5c58ebf7a68
Kendal Harland <kharland2011@gmail.com>
2015-03-09 10:48:40 -0500

    removed git commands from setup.hs

--- setup.sh
+++ setup.sh
@@ -1,6 +1,2 @@
 # Add rentalcar source libraries and extra libraries to classpath
 export CLASSPATH=$(pwd)/src:$(pwd)/src/swingx-all-1.6.4.jar
-
-# Initialize and update any submodules used for this project
-git submodule init
-git submodule update
