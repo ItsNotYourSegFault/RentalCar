365e7121a6d111f8dc34536e2f5698fa0689b338
Kendal Harland <kharland2011@gmail.com>
2015-03-09 10:47:55 -0500

    added swingx lib to classpath in setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 # Add rentalcar source libraries and extra libraries to classpath
-export CLASSPATH=$(pwd)/src:
+export CLASSPATH=$(pwd)/src:$(pwd)/src/swingx-all-1.6.4.jar
 
 # Initialize and update any submodules used for this project
 git submodule init
