18ee7d72ae75f2c2693835ba11b95aba12e31e26
Jackson Montgomery <j573m923@ku.edu>
2015-03-11 20:46:34 -0500

    added Module.java to ui lib

---
+++ src/rentalcar/ui/Module.java
@@ -0,0 +1,7 @@
+/** Utility class that represents a module */
+import java.awt.Container;
+
+public class Module {
+  public Container load(Container initialContent) { return initialContent; }
+  public Container clear(Container initialContent) { return initialContent; }
+}
