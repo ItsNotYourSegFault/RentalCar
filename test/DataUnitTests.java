a68854ee38fa73a7be65d1cdb2c07e569c0fd0a8
Kendal Harland <kharland2011@gmail.com>
2015-04-03 11:03:20 -0500

    adding unit tests for Database.java

--- test/DataUnitTests.java
+++ test/DataUnitTests.java
@@ -1,23 +1,20 @@
+import java.util.Set;
+
 import rentalcar.web.JSONRequest;
+import rentalcar.system.Database;
 import rentalcar.data.FormObject;
 
-/*
-  DataUnitTests.java
-  --
-  Kendal Harland <kendaljharland@gmail.com>
-  Siddhant Sethi <siddhant.sethi91@gmail.com>
-  Stephanie Sammons <chirox90@gmail.com>
-  Grant Hays <granthays@gmail.com>
-  Jackson Montgomery <j573m923@ku.edu>
-  Lei Wang <l290w868@gmail.com>
-  Greg Pregulman <gpregulman@gmail.com>
-  --
-  Unit tests for the rentalcar.data package
- */
-  
+
 public class DataUnitTests {
   public static void main(String[] args) {
+    Database db = new Database();
     FormObject res = new FormObject();
+    res.Set("test", "value");
     System.out.println(res.ToQueryString());
+
+    Set locations = db.GetLocations();
+    System.out.println(locations);
+    Integer locationId = db.Locations.get(locations.toArray()[locations.size()-1]);
+    System.out.println(db.GetVehiclesByLocation(locationId));
   }
 }
