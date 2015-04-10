966a4c44833dc4cdf273bdda50079a1ba39323cd
Kendal Harland <kharland2011@gmail.com>
2015-04-10 12:12:16 -0500

    reorganized code

--- test/DataUnitTests.java
+++ test/DataUnitTests.java
@@ -1,4 +1,6 @@
 import java.util.Set;
+import java.util.Iterator;
+import java.util.HashMap;
 
 import rentalcar.web.JSONRequest;
 import rentalcar.system.Database;
@@ -15,19 +17,31 @@ public class DataUnitTests {
 
 
     /** Garbage */
-    FormObject res = new FormObject();
-    res.Set("test", "value");
-    System.out.println(res.ToQueryString());
+    // FormObject res = new FormObject();
+    // res.Set("test", "value");
+    // System.out.println(res.ToQueryString());
     
-    location = locations[5];
+    // location = locations[5];
 
     /**
      * Example of iterating over the vehicle class names and the number of
      * vehicles for that class at a location
      */
     
-    // Fetch the list of classes and their vehicle counts
+    //Fetch the list of classes and their vehicle counts
     System.out.println(db.GetReservationClassCount(1, "2016-01-01", "2015-01-01"));
     System.out.println(db.GetVehicleClassCount(1));
+    
+    HashMap<String, Integer> countsV = db.GetVehicleClassCount(1);
+    HashMap<String, Integer> countsR = db.GetReservedVehicleClassCount(1, "2016-01-01", "2015-01-01");
+    Iterator<String> it = countsV.keySet().iterator();
+    while (it.hasNext()) {
+      String nextKey = it.next();
+      System.out.print(nextKey+": Total ");
+      System.out.print(countsV.get(nextKey));
+      System.out.print(",  Reserved ");
+      System.out.println(countsR.get(nextKey));
+    }
+
   }
 }
