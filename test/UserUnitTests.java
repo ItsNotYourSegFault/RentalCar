232aacb27436190c56e8a5ef4e3b3046eeaebfaa
SidSTi <siddhant.sethi91@gmail.com>
2015-04-12 16:34:29 -0500

    Fixed the other two unit tests

--- test/UserUnitTests.java
+++ test/UserUnitTests.java
@@ -1,3 +1,5 @@
+package test;
+
 import java.util.List;
 import java.util.ArrayList;
 import src.org.json.JSONObject;
--- test/WebUnitTests.java
+++ test/WebUnitTests.java
@@ -1,3 +1,5 @@
+package test;
+
 import src.rentalcar.web.Request;
 import src.rentalcar.web.JSONRequest;
 
