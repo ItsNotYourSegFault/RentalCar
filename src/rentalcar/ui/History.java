daf643467423d3111c5086d3ac36112be3a639a4
SidSTi <siddhant.sethi91@gmail.com>
2015-04-12 22:15:37 -0500

    Minor change to History Module

--- src/rentalcar/ui/History.java
+++ src/rentalcar/ui/History.java
@@ -214,11 +214,12 @@ public class History extends JPanel {
      * this method should be invoked from the
      * event-dispatching thread.
      */
-    private static void createAndShowGUI() {
+    static void createAndShowGUI() {
         //Create and set up the window.
 
         JFrame frame = new JFrame("History");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        //frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         //frame.setSize(800,600);
         frame.setBounds(100, 100, 800, 600);
         
@@ -237,7 +238,7 @@ public class History extends JPanel {
         frame.setVisible(true);
     }
 
-    public static void main(String[] args) {
+    /*public static void main(String[] args) {
         //Schedule a job for the event-dispatching thread:
         //creating and showing this application's GUI.
         javax.swing.SwingUtilities.invokeLater(new Runnable() {
@@ -246,5 +247,5 @@ public class History extends JPanel {
                 createAndShowGUI();
             }
         });
-    }
+    }*/
 }
