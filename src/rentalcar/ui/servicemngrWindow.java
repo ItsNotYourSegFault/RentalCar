49c7b4be0ecedf23cfc5c2d8bb23c2de81b6c68c
Kendal Harland <kharland2011@gmail.com>
2015-04-22 10:38:48 -0500

    made active jframe public so window can be launched from Login

--- src/rentalcar/ui/servicemngrWindow.java
+++ src/rentalcar/ui/servicemngrWindow.java
@@ -23,7 +23,7 @@ public class servicemngrWindow extends JPanel {
 	
 	private boolean DEBUG = false;
 
-	private JFrame frmServiceManager;
+	public JFrame frmServiceManager;
 	private final JCheckBox checkBox = new JCheckBox("Changed Oil");
 	private JTable table;
 	private JScrollBar scrollBar;
