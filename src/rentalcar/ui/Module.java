c4d77537d56b8d25e57a065fea18767617bc343e
SidSTi <siddhant.sethi91@gmail.com>
2015-04-21 19:55:27 -0500

    Removed unwanted files

--- src/rentalcar/ui/Module.java
+++
@@ -1,8 +0,0 @@
-/** Utility class that represents a module */
-package rentalcar.ui;
-import java.awt.Container;
-
-public class Module {
-  public Container load(Container initialContent) { return initialContent; }
-  public Container clear(Container initialContent) { return initialContent; }
-}
